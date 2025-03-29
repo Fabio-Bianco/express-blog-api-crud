f501c79bbcdb610c1942ad44dff9234feb8cc1bd
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-29 13:52:43 +0100

    Imported posts array from external file for use in /posts route

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const express = require("express"); // importazione di express
 const app = express(); // invocazione di express;
 const port = 4000; // variabile che identifica la porta;
 
+
+const posts = require('./posts'); // importazione dell'array dei post
+
 // aggiungo la rotta home page blog
 app.get("/", (req, res) => {
-  console.log(" Rotta principale del blog");
+  console.log(" rotta principale del blog");
 
   res.send("Benvenuto sul blog"); // risposta
 });
 
 // aggiungo una rotta per i post
 
-app.get("/posts", (req, res) => {
-  console.log(" Rotta principale i posts"); // rotta che deve risp ad una arrow function con una richiesta ed una risposta
-
-  res.send("Tutti i posts");
-});
+// rotta che restituisce tutti i post
+app.get('/posts', (req, res) => {
+    console.log('Rotta per i posts');
+   
+    res.json(posts); // restituisce l'array in formato JSON
+  });
 
 app.listen(port, () => {
   console.log("server attivo sulla porta " + port);
